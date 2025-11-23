f92c59ffe52eba92d052f38b1a38f98fc1266653
Johnwang2046little <johnwang2046@outlook.com>
2025-11-23 18:42:49 +0800

    Implement CORS headers in Twikoo function
    Add CORS support to Twikoo function handler

--- netlify/functions/twikoo.js
+++ netlify/functions/twikoo.js
@@ -1,1 +1,32 @@
-exports.handler = require('twikoo-netlify').handler
+const CORS_HEADERS = {
+  "Access-Control-Allow-Origin": "http://localhost:4321",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, DELETE",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Credentials": "true"
+};
+
+exports.handler = async (event) => {
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 204,
+      headers: CORS_HEADERS,
+      body: ""
+    };
+  }
+
+  // 你的正常处理
+  try {
+    const body = { message: "ok" };
+    return {
+      statusCode: 200,
+      headers: { ...CORS_HEADERS, "Content-Type": "application/json" },
+      body: JSON.stringify(body)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: CORS_HEADERS,
+      body: "error"
+    };
+  }
+};
